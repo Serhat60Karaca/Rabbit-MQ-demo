5c555d0fb09007a7149fe8aad68c0af477091739
Serhat60Karaca <91076040+Serhat60Karaca@users.noreply.github.com>
2023-05-19 16:25:36 +0300

    burda

---
+++ Gonder/Gonder/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MesajGonder();
+            }
+        }
+        public static void MesajGonder()
+        {
+            var factory = new ConnectionFactory();//rabbit mq(host) ya bağlanmak için kullanılır
+            factory.Uri = new Uri("amqps://avyxqfbb:ym0Ukt7rJTj5-TCiU38ZfOrkDDlW6pOV@sparrow.rmq.cloudamqp.com/avyxqfbb");//Url yapıştırdık
+
+            var connection = factory.CreateConnection();//bağlantı oluştur
+
+            var channel = connection.CreateModel();//channel oluşyur
+
+            channel.QueueDeclare("mesaz-kuyruk", true, false, false);//kuyruk adı, memory dışında tutulsun mu, bağlantıya dışardan erişilebilsin mi,alıcı bağlantıyı terk ettiğinde mesaj silinsin mi
+            var message = "mesaz deneme basarili";//gönderilecek mesaj
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(String.Empty, "mesaz-kuyruk", null, body);//kuyruğu kanala ekledik
+            Console.WriteLine("mesaz tavsan");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Gonder/MesajıYakala/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesajıYakala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://avyxqfbb:ym0Ukt7rJTj5-TCiU38ZfOrkDDlW6pOV@sparrow.rmq.cloudamqp.com/avyxqfbb");
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("mesaz-kuyruk", true, false, false);
+            var consumer = new EventingBasicConsumer(channel);//kuyruktaki yapıyı yakalamak için
+            channel.BasicConsume("mesaz-kuyruk",true, consumer);
+
+            consumer.Received += Consumer_Received;
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine("Gelen mesaz" + Encoding.UTF8.GetString(e.Body.ToArray()));
+        }
+    }
+}
